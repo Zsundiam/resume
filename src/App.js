8983b30bfa09331f3633b75dc5e8635a45ebc713
Zsundiam <zjmsundiam.student@ua.edu.ph>
2024-03-22 01:12:26 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import React from 'react';
+import Resume from './resume'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Resume /> {/* Render the Resume component */}
+    </div>
+  );
+}
+
+export default App;
