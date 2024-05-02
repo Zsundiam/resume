149cdf6d1d030d362988e24ec12967707e183221
Zsundiam <zjmsundiam.student@ua.edu.ph>
2024-05-02 23:10:23 +0800

    Updated

--- Calculator CPE-2A/src/App.js
+++ Calculator CPE-2A/src/App.js
@@ -4,37 +4,19 @@ import './App.css';
 function App() {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
-  const [operator, setOperator] = useState('');
-  const [prevInput, setPrevInput] = useState('');
 
   const handleClick = (value) => {
-    if (value === 'EQ') {
+    if (value === '=') {
       try {
-        const res = eval(prevInput + operator + input);
-        setResult(res.toString());
-        setInput(res.toString());
-        setPrevInput(res.toString());
+        setResult(eval(input).toString());
       } catch (error) {
         setResult('Error');
       }
     } else if (value === 'C') {
       setInput('');
       setResult('');
-      setOperator('');
-      setPrevInput('');
-    } else if (value === 'ADD' || value === 'SUB' || value === 'MUL' || value === 'DIV') {
-      if (input === '0') {
-        setInput('');
-      }
-      setOperator(value.replace('ADD', '+').replace('SUB', '-').replace('MUL', '*').replace('DIV', '/'));
-      setPrevInput(input);
-      setInput('');
     } else {
-      if (input === '0') {
-        setInput(value);
-      } else {
-        setInput(input + value);
-      }
+      setInput(input + value);
     }
   };
 
@@ -48,20 +30,20 @@ function App() {
         <button onClick={() => handleClick('7')}>7</button>
         <button onClick={() => handleClick('8')}>8</button>
         <button onClick={() => handleClick('9')}>9</button>
-        <button className="operator" onClick={() => handleClick('ADD')}>ADD</button>
+        <button className="operator" onClick={() => handleClick('+')}>+</button>
         <button onClick={() => handleClick('4')}>4</button>
         <button onClick={() => handleClick('5')}>5</button>
         <button onClick={() => handleClick('6')}>6</button>
-        <button className="operator" onClick={() => handleClick('SUB')}>SUB</button>
+        <button className="operator" onClick={() => handleClick('-')}>-</button>
         <button onClick={() => handleClick('1')}>1</button>
         <button onClick={() => handleClick('2')}>2</button>
         <button onClick={() => handleClick('3')}>3</button>
-        <button className="operator" onClick={() => handleClick('MUL')}>MUL</button>
+        <button className="operator" onClick={() => handleClick('*')}>*</button>
         <button onClick={() => handleClick('0')}>0</button>
         <button onClick={() => handleClick('.')}>.</button>
-        <button className="equal" onClick={() => handleClick('EQ')}>EQ</button>
-        <button className="operator" onClick={() => handleClick('DIV')}>DIV</button>
-        <button className="clear" onClick={() => handleClick('C')}>CLR</button>
+        <button className="equal" onClick={() => handleClick('=')}>=</button>
+        <button className="operator" onClick={() => handleClick('/')}>/</button>
+        <button className="clear" onClick={() => handleClick('C')}>C</button>
       </div>
     </div>
   );
